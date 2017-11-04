8cb1a3c639affb26d2626e3d31336d66f1588eda
ibrah <ibrah@139.179.224.182>
2017-11-04 12:43:53 +0300

    Deneme

--- src/Database/Database.java
+++ src/Database/Database.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package besttrade;
+package Database;
 
 /**
  *
  * @author ibrah
  */
-public class BestTrade {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Database {
     
 }
